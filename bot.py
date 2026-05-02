63bdfc78599d54e20c9fa0563ad44bc993f0df5d
farel453534 <94860046+farel453534@users.noreply.github.com>
2026-05-02 15:19:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4839,14 +4839,22 @@ async def info_command(interaction: discord.Interaction, user: discord.Member):
 async def gerants_command(interaction: discord.Interaction):
     try:
         description = (
-            "**Gérants de toutes les factions** : <@1413486076332605481> & <@404799720305983497>\n\n"
-            "> **__AURORS__** : <@1413486076332605481> et <@565773187116302346>\n"
-            "> **__MANGEMORT__** : <@1413486076332605481> et <@484798244996644864> & <@1045815146511081542>\n"
-            "> **__VAMPIRE__** : <@879458572986105887>\n"
-            "> **__MINISTERE__** : <@665228481654947853>\n"
-            "> **__MAGE-INDEPENDANT__** : <@665228481654947853>\n"
-            "> **__ORDRE DU PHENIX__** : <@380059243451121664>\n"
-            "> **__PROFESSEUR__** : <@415225558612443136> et <@685885648762044449>"
+            "## Gérants de toutes les factions\n"
+            "<@1413486076332605481> & <@404799720305983497>\n\n"
+            "## AURORS\n"
+            "<@1413486076332605481> et <@565773187116302346>\n\n"
+            "## MANGEMORT\n"
+            "<@1413486076332605481> et <@484798244996644864> & <@1045815146511081542>\n\n"
+            "## VAMPIRE\n"
+            "<@879458572986105887>\n\n"
+            "## MINISTERE\n"
+            "<@665228481654947853>\n\n"
+            "## MAGE-INDEPENDANT\n"
+            "<@665228481654947853>\n\n"
+            "## ORDRE DU PHENIX\n"
+            "<@380059243451121664>\n\n"
+            "## PROFESSEUR\n"
+            "<@415225558612443136> et <@685885648762044449>"
         )
         embed = discord.Embed(
             title="Gérants Whitelist Factions",
